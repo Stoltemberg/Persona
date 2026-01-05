02e807838be485d62359c68959dd12f0fb93f10b
Gabriel Stoltemberg <stoltemberg.stoltemberg@gmail.com>
2026-01-05 00:58:50 -0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,32 @@ import Analysis from './pages/Analysis';
 import Settings from './pages/Settings';
 import { Layout } from './components/Layout';
 
-// ... (ProtectedRoute etc)
+// Protected Route Wrapper
+const ProtectedRoute = () => {
+  const { user, loading } = useAuth();
 
-// In Routes:
+  if (loading) return <div className="flex-center" style={{ height: '100vh' }}>Loading...</div>;
+
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Layout />}>
+            <Route path="/" element={<Dashboard />} />
             <Route path="/transactions" element={<Transactions />} />
             <Route path="/analysis" element={<Analysis />} />
             <Route path="/goals" element={<Goals />} />
             <Route path="/settings" element={<Settings />} />
-          </Route >
-        </Route >
-      </Routes >
-    </Router >
+          </Route>
+        </Route>
+      </Routes>
+    </Router>
   );
 }
 

9127fc1bf2d16f65635006e12bc1d99ef76f9750
Gabriel Stoltemberg <stoltemberg.stoltemberg@gmail.com>
2026-01-08 00:32:39 -0300

    .

--- src/components/DateRangePicker.jsx
+++ src/components/DateRangePicker.jsx
@@ -118,6 +118,7 @@ export function DateRangePicker({ startDate, endDate, onChange }) {
                             transition={{ duration: 0.15 }}
                             className="glass-card"
                             style={{
+                                position: 'absolute',
                                 top: coords.top,
                                 left: coords.left,
                                 transform: coords.transform, // Add transform
